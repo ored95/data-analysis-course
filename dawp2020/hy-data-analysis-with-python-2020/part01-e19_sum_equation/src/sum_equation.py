de59e3e710b5dca530d163e8865d2b515c2258d4
ored95 <stepup.ored@gmail.com>
2021-03-10 18:04:14 +0300

    Part 01 e19 done

--- dawp2020/hy-data-analysis-with-python-2020/part01-e19_sum_equation/src/sum_equation.py
+++ dawp2020/hy-data-analysis-with-python-2020/part01-e19_sum_equation/src/sum_equation.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 def sum_equation(L):
-    return ""
+    if not L:
+        return "0 = 0"
+    return '{0} = {1}'.format(' + '.join(map(str, L)), sum(L))
 
 def main():
+    print(sum_equation([1, 5, 7]))
     pass
 
 if __name__ == "__main__":
