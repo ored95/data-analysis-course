107f2f364d95cd73986d084afb08c20007a729fc
ored95 <stepup.ored@gmail.com>
2021-03-10 16:49:28 +0300

    Part 01 e14 done

--- dawp2020/hy-data-analysis-with-python-2020/part01-e14_find_matching/src/find_matching.py
+++ dawp2020/hy-data-analysis-with-python-2020/part01-e14_find_matching/src/find_matching.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+# Hint: The enumerate(L) function call can be thought to be equivalent to zip(range(len(L)), L).
 
 def find_matching(L, pattern):
-    return []
+    return [i for i, _ in enumerate(L) if pattern in L[i]]
 
 def main():
     pass
