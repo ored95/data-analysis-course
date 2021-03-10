6ba69421a382f3b86ff66a88fcae3f75cd779dd9
ored95 <stepup.ored@gmail.com>
2021-03-10 14:57:34 +0300

    Part 01 e9 done

--- dawp2020/hy-data-analysis-with-python-2020/part01-e09_merge/src/merge.py
+++ dawp2020/hy-data-analysis-with-python-2020/part01-e09_merge/src/merge.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 
 def merge(L1, L2):
-    return []
+    L = [] 
+    i, j = 0, 0
+  
+    while i < len(L1) and j < len(L2): 
+        if L1[i] < L2[j]: 
+            L.append(L1[i]) 
+            i += 1
+        else: 
+            L.append(L2[j]) 
+            j += 1
 
+    L = L + L1[i:] + L2[j:]
+    return L
+    
 def main():
     pass
 
