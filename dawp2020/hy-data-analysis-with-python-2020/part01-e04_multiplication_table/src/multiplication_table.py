fd2415ecac1f26f261b3c53cc90e423ac990c58b
ored95 <stepup.ored@gmail.com>
2021-03-10 14:11:57 +0300

    Part 01 e4 done

--- dawp2020/hy-data-analysis-with-python-2020/part01-e04_multiplication_table/src/multiplication_table.py
+++ dawp2020/hy-data-analysis-with-python-2020/part01-e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,10 @@
 
 
 def main():
-    pass
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print('{0:4d}'.format(i * j), end="")
+        print()
 
 if __name__ == "__main__":
     main()
