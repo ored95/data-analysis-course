acc0df162c10c989ccda1bf70f55879eb4d71225
ored95 <stepup.ored@gmail.com>
2021-03-10 14:07:27 +0300

    Part 01 e3 done

--- dawp2020/hy-data-analysis-with-python-2020/part01-e03_multiplication/src/multiplication.py
+++ dawp2020/hy-data-analysis-with-python-2020/part01-e03_multiplication/src/multiplication.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def main():
-    # Enter your solution here
+    for i in range(11):
+        print('4 multiplied by {0} is {1}'.format(i, i * 4))
 
 if __name__ == "__main__":
     main()
