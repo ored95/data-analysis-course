9fc2dc718ce333ecd5f02ec5453b746af69b9bf8
ored95 <stepup.ored@gmail.com>
2021-03-10 16:16:17 +0300

    Part 01 e12 done

--- dawp2020/hy-data-analysis-with-python-2020/part01-e12_distinct_characters/src/distinct_characters.py
+++ dawp2020/hy-data-analysis-with-python-2020/part01-e12_distinct_characters/src/distinct_characters.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def distinct_characters(L):
-    return {}
+    d = {}
+    for item in L:
+        d[item] = len(set(item))
+    return d
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
