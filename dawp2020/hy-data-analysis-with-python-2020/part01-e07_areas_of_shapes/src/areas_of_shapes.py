a6cc142f01d2a0f8b796597de27eb7b7b750e570
ored95 <stepup.ored@gmail.com>
2021-03-10 14:38:34 +0300

    Part 01 e7 done

--- dawp2020/hy-data-analysis-with-python-2020/part01-e07_areas_of_shapes/src/areas_of_shapes.py
+++ dawp2020/hy-data-analysis-with-python-2020/part01-e07_areas_of_shapes/src/areas_of_shapes.py
@@ -4,7 +4,23 @@ import math
 
 
 def main():
-    # enter you solution here
+    while True:
+        choice = str(input('Choose a shape (triangle, rectangle, circle): '))
+        if choice == '':
+            break
+        elif choice == 'triangle':
+            base   = float(input('Give base of the triangle: '))
+            height = float(input('Give height of the triangle: '))
+            print('The area is {0}'.format(0.5 * base * height))
+        elif choice == 'rectangle':
+            width = float(input('Give width of the rectangle: '))
+            height = float(input('Give height of the rectangle: '))
+            print('The area is {0}'.format(width * height))
+        elif choice == 'circle':
+            radius = float(input('Give radius of the circle: '))
+            print('The area is {0}'.format(math.pi * radius ** 2))
+        else:
+            print('Unknown shape!')
 
 if __name__ == "__main__":
     main()
