c27f4a6459e49f9448cf2624ea6440c1d8cddf88
ored95 <stepup.ored@gmail.com>
2021-03-10 16:10:13 +0300

    Part 01 e11 done

--- dawp2020/hy-data-analysis-with-python-2020/part01-e11_interleave/src/interleave.py
+++ dawp2020/hy-data-analysis-with-python-2020/part01-e11_interleave/src/interleave.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def interleave(*lists):
-    return []
+    return [item for set in zip(*lists) for item in set]
 
 def main():
     print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
