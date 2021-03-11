267f49d985114d9ac3956fe0e3a9be03a2749d09
ored95 <stepup.ored@gmail.com>
2021-03-11 18:57:42 +0300

    Part 02 e1 done

--- dawp2020/hy-data-analysis-with-python-2020/part02-e01_integers_in_brackets/src/integers_in_brackets.py
+++ dawp2020/hy-data-analysis-with-python-2020/part02-e01_integers_in_brackets/src/integers_in_brackets.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+import re
 
 def integers_in_brackets(s):
-    return []
+    N = re.findall(r'\[\s*?[+-]?\d+\s*?\]', s)
+    return [int(x[1:-1]) for x in N]
 
 def main():
+    print(integers_in_brackets("  afd [asd] [12 ] [a34]  [      -43 ]tt [+12]xxx"))
     pass
 
 if __name__ == "__main__":
