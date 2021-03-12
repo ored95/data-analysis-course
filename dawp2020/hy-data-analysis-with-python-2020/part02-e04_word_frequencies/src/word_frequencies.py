f508efa0762506622b4f4ba285d06408649825e6
ored95 <stepup.ored@gmail.com>
2021-03-13 00:10:44 +0300

    Part 02 e4 done

--- dawp2020/hy-data-analysis-with-python-2020/part02-e04_word_frequencies/src/word_frequencies.py
+++ dawp2020/hy-data-analysis-with-python-2020/part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
+'''
+Create function word_frequencies that gets a filename as a parameter and returns a dict 
+with the word frequencies. In the dictionary the keys are the words and the corresponding 
+values are the number of times that word occurred in the file specified by the function 
+parameter. Read all the lines from the file and split the lines into words using the split() method. 
+Further, remove punctuation from the ends of words using the strip("""!"#$%&'()*,-./:;?@[]_""") method call.
+'''
 
-def word_frequencies(filename):
-    return {}
+def word_frequencies(filename='src/alice.txt'):
+    dict = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            words = [w.strip("""!"#$%&'()*,-./:;?@[]_""") for w in line.split()]
+            for word in words:
+                if word in dict.keys():
+                    dict[word] += 1
+                else:
+                    dict[word] = 1
+    return dict
 
 def main():
     pass
