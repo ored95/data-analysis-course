5d648203ad242995df79a3468ae8139e8cb54b06
ored95 <stepup.ored@gmail.com>
2021-03-14 00:27:47 +0300

    Part 02 e13 done

--- dawp2020/hy-data-analysis-with-python-2020/part02-e13_diamond/src/diamond.py
+++ dawp2020/hy-data-analysis-with-python-2020/part02-e13_diamond/src/diamond.py
@@ -3,9 +3,16 @@
 import numpy as np
 
 def diamond(n):
-    return np.array([])
+    if n < 0:
+        return None
+    _id = np.eye(n, dtype=int)      # identity matrix (diagonal)
+    rid = _id[::-1]                 # reversed | create an anti-diagonal matrix
+    left    = np.concatenate((rid, _id[1:]), axis=0)
+    right   = np.concatenate((_id, rid[1:]), axis=0)
+    return np.concatenate((left.T, right.T[1:]), axis=0)
 
 def main():
+    print(diamond(1))
     pass
 
 if __name__ == "__main__":
