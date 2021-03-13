48bf9755d2b8dad778eacb8b5408b4c785ebc35b
ored95 <stepup.ored@gmail.com>
2021-03-13 15:00:06 +0300

    Part 02 e9 done

--- dawp2020/hy-data-analysis-with-python-2020/part02-e09_rational/src/rational.py
+++ dawp2020/hy-data-analysis-with-python-2020/part02-e09_rational/src/rational.py
@@ -1,7 +1,41 @@
 #!/usr/bin/env python3
 
+from os import supports_bytes_environ
+
+
 class Rational(object):
-    pass
+    def __init__(self, a, b) -> None:
+        if b == 0:
+            return None     # ZeroDivisor
+        self.a = a
+        self.b = b
+    
+    def __str__(self) -> str:
+        return '{0}/{1}'.format(self.a, self.b)
+
+    def __mul__(self, o: object) -> object:
+        return Rational(self.a * o.a, self.b * o.b)
+
+    def __truediv__(self, o: object) -> object:
+        if o.a == 0:
+            return None
+        return Rational(self.a * o.b, self.b * o.a)
+
+    def __add__(self, o: object) -> object:
+        return Rational(self.a * o.b + self.b * o.a, self.b * o.b)
+
+    def __sub__(self, o: object) -> object:
+        return Rational(self.a * o.b - self.b * o.a, self.b * o.b)
+
+    def __eq__(self, o: object) -> bool:
+        return self.a == o.a and self.b == o.b
+
+    def __gt__(self, o: object) -> bool:
+        return self.a * o.b > self.b * o.a
+    
+    def __lt__(self, o: object) -> bool:
+        return self.a * o.b < self.b * o.a
+
 
 def main():
     r1=Rational(1,4)
