efdaf51fdaa5dc34c80fec89e3c2ce46ea2eb8d9
ored95 <stepup.ored@gmail.com>
2021-03-14 02:20:13 +0300

    Part 03 e2 done

--- dawp2020/hy-data-analysis-with-python-2020/part03-e02_first_half_second_half/src/first_half_second_half.py
+++ dawp2020/hy-data-analysis-with-python-2020/part03-e02_first_half_second_half/src/first_half_second_half.py
@@ -3,7 +3,10 @@
 import numpy as np
 
 def first_half_second_half(a):
-    return np.array([])
+    first, second = np.split(a, 2, axis=1)
+    # (first version): c = first.sum(axis=1) > second.sum(axis=1)
+    c = np.sum(first, axis=1) > np.sum(second, axis=1)
+    return a[c]
 
 def main():
     pass
