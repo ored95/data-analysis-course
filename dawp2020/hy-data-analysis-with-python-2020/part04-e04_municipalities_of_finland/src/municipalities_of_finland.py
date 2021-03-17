7355529c258632336d1fbaca77e947a08a2d34ca
ored95 <stepup.ored@gmail.com>
2021-03-17 18:53:27 +0300

    Part 04 e4 done

--- dawp2020/hy-data-analysis-with-python-2020/part04-e04_municipalities_of_finland/src/municipalities_of_finland.py
+++ dawp2020/hy-data-analysis-with-python-2020/part04-e04_municipalities_of_finland/src/municipalities_of_finland.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-
+import numpy as np
 def municipalities_of_finland():
-    return None
+    df = pd.read_csv('src/municipal.tsv', sep='\t')
+    regions = list(df['Region 2018'])
+    begin, end = regions.index('Akaa'), regions.index('Äänekoski')
+    df = df.drop(['Region 2018'], axis=1)[begin:end+1]
+    df.index = regions[begin:end+1]
+    return df
     
 def main():
-    return
+    print(municipalities_of_finland())
     
 if __name__ == "__main__":
     main()
