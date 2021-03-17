5643ac378f0f86810acda5c94ab80d900000239a
ored95 <stepup.ored@gmail.com>
2021-03-17 17:41:34 +0300

    Part 04 e2 done

--- dawp2020/hy-data-analysis-with-python-2020/part04-e02_powers_of_series/src/powers_of_series.py
+++ dawp2020/hy-data-analysis-with-python-2020/part04-e02_powers_of_series/src/powers_of_series.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 
 def powers_of_series(s, k):
-    return None
+    d = {}
+    for i in range(1, k+1):
+        d[i] = np.power(s.values, i)
+    return pd.DataFrame(d, index=s.index)
     
 def main():
-    return
+    s = pd.Series([1,2,3,4], index=list("abcd"))
+    print(powers_of_series(s, 3))
     
 if __name__ == "__main__":
     main()
