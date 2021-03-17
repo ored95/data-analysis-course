29c8cda4e09ae7da4221e172cd64f7031e17d3ec
ored95 <stepup.ored@gmail.com>
2021-03-17 17:47:57 +0300

    Part 04 e3 done

--- dawp2020/hy-data-analysis-with-python-2020/part04-e03_municipal_information/src/municipal_information.py
+++ dawp2020/hy-data-analysis-with-python-2020/part04-e03_municipal_information/src/municipal_information.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+from numpy.core.fromnumeric import shape
 import pandas as pd
 
 def main():
-    return
+    df = pd.read_csv('src/municipal.tsv', sep='\t')
+    print('Shape: {},{}'.format(df.shape[0], df.shape[1]))
+    print('Columns:')
+    for c in df.columns:
+        print(c)
 
 
 if __name__ == "__main__":
