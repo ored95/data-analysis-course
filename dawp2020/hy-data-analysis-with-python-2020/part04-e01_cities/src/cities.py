f789ddbf723f5d1d5ff97e2614b15991d4b8603f
ored95 <stepup.ored@gmail.com>
2021-03-17 17:32:28 +0300

    Part 04 e1 done

--- dawp2020/hy-data-analysis-with-python-2020/part04-e01_cities/src/cities.py
+++ dawp2020/hy-data-analysis-with-python-2020/part04-e01_cities/src/cities.py
@@ -3,10 +3,16 @@
 import pandas as pd
 
 def cities():
-    return None
-    
+    return pd.DataFrame(
+        {
+            'Population':[643272, 279044, 231853, 223027, 201810],
+            'Total area':[715.48, 528.03, 689.59, 240.35, 3817.52]
+        }, 
+        index=['Helsinki', 'Espoo', 'Tampere', 'Vantaa', 'Oulu']
+    )
+
 def main():
-    return
+    print(cities())
     
 if __name__ == "__main__":
     main()
