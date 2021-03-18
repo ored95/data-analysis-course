ec58fa8433bab78ee785c7d916d0382db9f9b66e
ored95 <stepup.ored@gmail.com>
2021-03-18 14:24:05 +0300

    Part 04 e13 done

--- dawp2020/hy-data-analysis-with-python-2020/part04-e13_missing_value_types/src/missing_value_types.py
+++ dawp2020/hy-data-analysis-with-python-2020/part04-e13_missing_value_types/src/missing_value_types.py
@@ -4,10 +4,13 @@ import pandas as pd
 import numpy as np
 
 def missing_value_types():
-    return None
+    state    = pd.Series(np.array(['United Kingdom',  'Finland',   'USA', 'Sweden',    'Germany', 'Russia']), name='State')
+    yearOfIndependence = np.array([          np.nan,       1917,    1776,     1523,       np.nan,     1992], dtype=np.float64)
+    president          = np.array([          np.nan, 'Niinistö', 'Trump',   np.nan, 'Steinmeier',  'Putin'], dtype=object)
+    return pd.DataFrame({'Year of independence': yearOfIndependence, 'President': president}, index=state)
                
 def main():
-    return
+    print(missing_value_types())
 
 if __name__ == "__main__":
     main()
