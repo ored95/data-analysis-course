0b2a8e73bce7bc720b757eeb0e932105a10fdd16
ored95 <stepup.ored@gmail.com>
2021-03-18 13:06:14 +0300

    Part 04 e10 done

--- dawp2020/hy-data-analysis-with-python-2020/part04-e10_average_temperature/src/average_temperature.py
+++ dawp2020/hy-data-analysis-with-python-2020/part04-e10_average_temperature/src/average_temperature.py
@@ -3,10 +3,12 @@
 import pandas as pd
 
 def average_temperature():
-    return 0.0
+    df = pd.read_csv('src/kumpula-weather-2017.csv')
+    df = df[df['m'] == 7]
+    return df.iloc[:,-1].mean()
 
 def main():
-    return
+    print('Average temperature in July: {:.1f}'.format(average_temperature()))
 
 if __name__ == "__main__":
     main()
