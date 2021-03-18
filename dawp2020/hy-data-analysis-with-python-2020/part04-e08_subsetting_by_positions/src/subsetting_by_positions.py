e7678f56c95f966f3295d72a932e8622c2df1c84
ored95 <stepup.ored@gmail.com>
2021-03-18 12:54:48 +0300

    Part 04 e8 done

--- dawp2020/hy-data-analysis-with-python-2020/part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
+++ dawp2020/hy-data-analysis-with-python-2020/part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
@@ -3,10 +3,11 @@
 import pandas as pd
 
 def subsetting_by_positions():
-    return pd.DataFrame()
+    df = pd.read_csv('src/UK-top40-1964-1-2.tsv', index_col=0, sep='\t')
+    return df.iloc[:10,[1, 2]]
 
 def main():
-    return
+    print(subsetting_by_positions())
 
 if __name__ == "__main__":
     main()
