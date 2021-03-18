0bc010aebe58b029f573d5a60ac3739b492b25ee
ored95 <stepup.ored@gmail.com>
2021-03-18 13:55:43 +0300

    Part 04 e12 done

--- dawp2020/hy-data-analysis-with-python-2020/part04-e12_cyclists/src/cyclists.py
+++ dawp2020/hy-data-analysis-with-python-2020/part04-e12_cyclists/src/cyclists.py
@@ -3,11 +3,14 @@
 import pandas as pd
 
 def cyclists():
-    return None
-
+    df = pd.read_csv('src/Helsingin_pyorailijamaarat.csv', sep=';')
+    # [Same as line below]: df = df[df.notna().any(axis=1)]
+    df = df.dropna(how='all')
+    df = df.dropna(how='all', axis=1)
+    return df
 
 def main():
-    return
+    print(cyclists())
     
 if __name__ == "__main__":
     main()
