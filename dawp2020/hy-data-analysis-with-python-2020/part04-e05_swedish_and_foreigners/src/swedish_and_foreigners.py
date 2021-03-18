42ac8e6038039ee931f5ede2e70fb4fc8a7b13e4
ored95 <stepup.ored@gmail.com>
2021-03-18 12:43:21 +0300

    Part 04 e4-5 updated

--- dawp2020/hy-data-analysis-with-python-2020/part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
+++ dawp2020/hy-data-analysis-with-python-2020/part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
@@ -2,16 +2,8 @@
 
 import pandas as pd
 
-def municipalities_of_finland():
-    df = pd.read_csv('src/municipal.tsv', sep='\t')
-    regions = list(df['Region 2018'])
-    begin, end = regions.index('Akaa'), regions.index('Äänekoski')
-    df = df.drop(['Region 2018'], axis=1)[begin:end+1]
-    df.index = regions[begin:end+1]
-    return df
-
 def swedish_and_foreigners():
-    df = municipalities_of_finland()
+    df = pd.read_csv('src/municipal.tsv', index_col=0, sep='\t')['Akaa':'Äänekoski']
     # choice = (df['Share of Swedish-speakers of the population, %'] > 5) & (df['Share of foreign citizens of the population, %'] > 5)
     choice = (df[df.columns[2]] > 5) & (df[df.columns[3]] > 5)
     df = df[choice]
