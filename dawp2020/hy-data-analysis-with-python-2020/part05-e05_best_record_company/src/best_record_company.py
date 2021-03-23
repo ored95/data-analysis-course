9172da8b04a8d996d8bd547a79e9f3c5750ad026
ored95 <stepup.ored@gmail.com>
2021-03-23 12:54:49 +0300

    Part 05 e5 done

--- dawp2020/hy-data-analysis-with-python-2020/part05-e05_best_record_company/src/best_record_company.py
+++ dawp2020/hy-data-analysis-with-python-2020/part05-e05_best_record_company/src/best_record_company.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
+from os import name, sep
 import pandas as pd
 
 def best_record_company():
-    return None
+    df = pd.read_csv('src/UK-top40-1964-1-2.tsv', sep='\t')
+    publisher = df.groupby('Publisher').sum().sort_values(['WoC'], ascending=False).iloc[0].name
+    return df[df['Publisher'] == publisher]
 
 def main():
-    return
+    print(best_record_company())
     
 
 if __name__ == "__main__":
