135a221f1e09de5b4310af7ff57c457fe8646dac
ored95 <stepup.ored@gmail.com>
2021-03-25 15:38:26 +0300

    Part 05 e12 updated

--- dawp2020/hy-data-analysis-with-python-2020/part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
+++ dawp2020/hy-data-analysis-with-python-2020/part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
@@ -1,29 +1,39 @@
-#!/usr/bin/env python3
-
-import numpy as np
 import pandas as pd
-from pandas.core.algorithms import mode
 from sklearn import linear_model
-
+ 
+ 
 def coefficient_of_determination():
-    df = pd.read_csv('src/mystery_data.tsv', sep='\t')
-    model = linear_model.LinearRegression(fit_intercept=True)
-    X = df[df.columns[:-1]]
-    Y = df[df.columns[-1]]
     scores = []
-    model.fit(X, Y)
-    scores.append(model.score(X, Y))
-    for x in df.columns[:-1]:
-        X = df[x].to_numpy()
-        model.fit(X[:, np.newaxis], Y)
-        scores.append(model.score(X[:, np.newaxis], Y))
+    df = pd.read_csv("src/mystery_data.tsv", sep="\t")
+    X = df.loc[:, "X1":"X5"]
+    y = df.Y
+ 
+    reg = linear_model.LinearRegression(fit_intercept=True)
+ 
+    reg.fit(X, y)
+    score = reg.score(X, y)
+    scores.append(score)
+ 
+    features = df.columns[:-1]
+    print(features)
+    for f in features:
+        x = df[[f]]
+        print(x.shape)
+        reg.fit(x, y)
+        scores.append(reg.score(x, y))
+        
     return scores
     
 def main():
     scores = coefficient_of_determination()
-    print('R2-score with feature(s) X: ', scores[0])
-    for i, s in enumerate(scores):
-        print('R2-score with feature(s) X{}: {:.4f}'.format(i, s))
-
+ 
+    z = scores[1:]
+    for i in range(4):
+        print(sum(z[i:i+2]))
+        
+    titles = "X X1 X2 X3 X4 X5".split()
+    for title, score in zip(titles, scores):
+        print(f"R2-score with feature(s) {title}: {score:.5f}")
+ 
 if __name__ == "__main__":
     main()
