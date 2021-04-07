0add1f457c5665e022b14a124da819e3714ff36a
ored95 <stepup.ored@gmail.com>
2021-04-07 11:00:11 +0300

    Part 04 e6 done

--- dawp2020/hy-data-analysis-with-python-2020/part04-e06_growing_municipalities/src/growing_municipalities.py
+++ dawp2020/hy-data-analysis-with-python-2020/part04-e06_growing_municipalities/src/growing_municipalities.py
@@ -2,16 +2,16 @@
 
 import pandas as pd
 
-c = 'Population change from the previous year, %'
-
 def growing_municipalities(df):
-    return df[df[c] > 0.0]
+    increasing_population_change = df[df.columns[1]] > 0
+    growing_count = df[increasing_population_change].shape[0]
+    proportion = growing_count / df.shape[0]
+    return proportion
 
 def main():
     df = pd.read_csv('src/municipal.tsv', index_col=0, sep='\t')
     df = df["Akaa":"Kannus"]
-    for i in growing_municipalities(df)[c]:
-        print('Proportion of growing municipalities: {:.1f}%'.format(i))
+    print('Proportion of growing municipalities: {:.1f}%'.format(growing_municipalities(df)))
 
 if __name__ == "__main__":
     main()
